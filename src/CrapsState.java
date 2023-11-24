973665d18e8c48f543b940a696a65cd3ddf7b695
atagunay <atafb2000gunay@gmail.com>
2023-11-24 16:03:34 +0000

    Update play method in CrapsGameState

--- src/CrapsState.java
+++ src/CrapsState.java
@@ -1,6 +1,15 @@
 class CrapsState extends CrapsGameState {
     void play(int rolled) {
         System.out.println("CrapsState.play() - calling on...");
-        super.play(rolled);
+        System.out.println("CrapsGameState.play()");
+        if (rolled == 7 || rolled == 11) {
+            // now has 2 params
+            GameController.setState("WIN", rolled);
+
+        } else if (rolled == 2 || rolled == 3 || rolled == 12) {
+            GameController.setState("LOOSE", rolled);
+        } else {
+            GameController.setState("POINT", rolled);
+        }
     }
 }
